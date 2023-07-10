a978c042ed12670d47e60100c4c15cb043054caa
김가은 <kge02160@gmail.com>
2023-07-10 20:12:41 +0900

    테스트

---
+++ src/main/java/global/error/exception/BusinessException.java
@@ -0,0 +1,10 @@
+package global.error.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class BusinessException extends RuntimeException {
+    private final ErrorCode errorCode;
+}
